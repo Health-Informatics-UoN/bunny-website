2c0f765c42152ac37d2926da4b3adecc23fee843
Andy Rae <hello@rae.li>
2025-02-20 20:56:32 +0000

    Fixes

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -10,23 +10,23 @@ export const Hero = () => {
     <div className="px-6 lg:px-8">
       <div className="mx-auto max-w-2xl py-32 sm:py-32 lg:py-24">
         <div className="mb-8 flex justify-center">
-          <img alt="" src="./logos/bunny1.png" className="h-30 w-auto" />
+          <img alt="" src="./logos/bunny1.svg" className="h-30 w-auto" />
         </div>
         <div className="text-center">
           <h1
             className={cn(
-              "text-2xl font-semibold text-balance text-white sm:text-5xl leading-loose",
+              "text-2xl font-semibold text-balance sm:text-5xl leading-loose",
               font.className
             )}
           >
             Unlock insights, simplify discovery.
           </h1>
-          <p className="text-gray-600 text-lg mt-10">
-            Your solution for quick, secure, open source Federated Discovery.
+          <p className="text-gray-500 text-lg mt-10">
+            Your solution for quick, secure, federated, open source Cohort Discovery.
           </p>
           <div className="mt-16 flex items-center justify-center gap-x-6">
-            <Link href="/bunny">
-              <ShimmerButton className="flex" borderRadius="2px" background="#4E46DD">
+            <Link href={process.env.NEXT_PUBLIC_BUNNY_LINK ?? ""}>
+              <ShimmerButton className="flex" background="#4BA6EE" shimmerSize="2px" shimmerDuration="2s">
                 <span
                   className={cn(
                     "flex items-center gap-2 opacity-90 whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-xl",
--- website/src/components/partners.tsx
+++ website/src/components/partners.tsx
@@ -3,16 +3,16 @@ export default function Partners() {
     <>
       <div className="lg:flex lg:mx-18 lg:p-8  lg:flex-row items-center lg:gap-5 flex flex-col gap-2 lg:justify-between dark:bg-white rounded-xl mx-10">
         <div className="w-[18%] aspect-3/2 object-contain">
-          <img src="/logos/uon.png" alt="Logo UoN" />
+          <img src="/logos/uon.png" alt="Logo UoN" className="opacity-60 hover:opacity-100 transition-all duration-300" />
         </div>
         <div className="w-[25%] aspect-3/2 object-contain">
-          <img src="/logos/nihr-nbrc.png" alt="Logo CCN" />
+          <img src="/logos/nihr-nbrc.png" alt="Logo CCN" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300 opacity-60 hover:opacity-100" />
         </div>
         <div className="w-[14%] aspect-3/2 object-contain">
-          <img src="/logos/hdr.png" alt="Logo UKRI" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300" />
+          <img src="/logos/hdr.png" alt="Logo UKRI" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300 opacity-60 hover:opacity-100" />
         </div>
         <div className="w-[12%] aspect-3/2 object-contain relative">
-          <img src="/logos/sde.png" alt="Logo ALVE" className="filter brightness-0 hover:filter-none transition-all duration-300" />
+          <img src="/logos/sde.png" alt="Logo ALVE" className="filter brightness-0 hover:filter-none transition-all duration-300 opacity-60 hover:opacity-100" />
         </div>
       </div>
     </>
