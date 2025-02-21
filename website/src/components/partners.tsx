ecaed04402149982b02a790e6f65588dc27f1d3e
Andy Rae <hello@rae.li>
2025-02-21 15:32:04 +0000

    Make mobile responsive

--- website/src/components/partners.tsx
+++ website/src/components/partners.tsx
@@ -1,18 +1,18 @@
 export default function Partners() {
   return (
     <>
-      <div className="lg:flex lg:mx-18 lg:p-8  lg:flex-row items-center lg:gap-5 flex flex-col gap-2 lg:justify-between rounded-xl mx-10">
-        <div className="w-[18%] aspect-3/2 object-contain">
-          <img src="/logos/uon.png" alt="Logo UoN" className="opacity-60 hover:opacity-100 transition-all duration-300" />
+      <div className="flex flex-col items-center gap-8 mx-10 lg:flex-row lg:justify-between lg:gap-5 lg:mx-18 lg:p-8 rounded-xl">
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-[18%] aspect-3/2 object-contain">
+          <img src="/logos/uon.png" alt="Logo UoN" className="opacity-50 hover:opacity-100 transition-all duration-300" />
         </div>
-        <div className="w-[25%] aspect-3/2 object-contain">
-          <img src="/logos/nihr-nbrc.png" alt="Logo CCN" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300 opacity-60 hover:opacity-100" />
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-[25%] aspect-3/2 object-contain">
+          <img src="/logos/nihr-nbrc.png" alt="Logo CCN" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300 opacity-50 hover:opacity-100" />
         </div>
-        <div className="w-[14%] aspect-3/2 object-contain">
-          <img src="/logos/hdr.png" alt="Logo UKRI" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300 opacity-60 hover:opacity-100" />
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-[14%] aspect-3/2 object-contain">
+          <img src="/logos/hdr.png" alt="Logo UKRI" className="filter brightness-0 dark:filter dark:brightness-100 dark:invert hover:filter-none transition-all duration-300 opacity-50 hover:opacity-100" />
         </div>
-        <div className="w-[12%] aspect-3/2 object-contain relative">
-          <img src="/logos/sde.png" alt="Logo ALVE" className="filter brightness-0 hover:filter-none transition-all duration-300 opacity-60 hover:opacity-100" />
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-[12%] aspect-3/2 object-contain relative">
+          <img src="/logos/sde.png" alt="Logo ALVE" className="filter brightness-0 hover:filter-none transition-all duration-300 opacity-50 hover:opacity-100" />
         </div>
       </div>
     </>
